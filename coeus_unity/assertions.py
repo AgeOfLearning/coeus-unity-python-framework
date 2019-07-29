ea0264867ea0f8ab351a08d24ea6789df353965c
Greg Morchower <greg.morchower@aofl.com>
2019-07-29 12:21:06 -0700

    Added support for transform references

--- coeus_unity/assertions.py
+++ coeus_unity/assertions.py
@@ -6,14 +6,14 @@ DEFAULT_RENDERER_VISIBLE = True
 DEFAULT_SCENE_LOADED = True
 
 
-def assert_transform_exists(cli, transform_path):
+def assert_transform_exists(cli, transform_ref):
     """
     Asserts that the transform exists.
     :param cli:
-    :param transform_path:
+    :param transform_ref:
     :return:
     """
-    result = commands.query_transform_exists(cli, transform_path)
+    result = commands.query_transform_exists(cli, transform_ref)
     assert result is True
     return result
 
@@ -30,62 +30,62 @@ def assert_scene_loaded(cli, scene_name):
     return result
 
 
-def assert_await_transform_exists(cli, transform_path, does_exist=DEFAULT_TRANSFORM_EXISTS, timeout_seconds=DEFAULT_TIMEOUT_SECONDS):
+def assert_await_transform_exists(cli, transform_ref, does_exist=DEFAULT_TRANSFORM_EXISTS, timeout_seconds=DEFAULT_TIMEOUT_SECONDS):
     """
     Asserts that we successfully awaited for the transform to exist based on does_exist. If the timeout passes
     or the expression is_registered != actual state, then it will fail.
     :param cli:
-    :param transform_path:
+    :param transform_ref:
     :param does_exist: (True | False) the state change we are waiting for.
     :param timeout_seconds: The amount of time to wait for a change before fail.
     :return:
     """
-    result = commands.await_transform_exists(cli, transform_path, does_exist, timeout_seconds)
+    result = commands.await_transform_exists(cli, transform_ref, does_exist, timeout_seconds)
     assert result is True
     return result
 
 
-def assert_await_any_transforms_exist(cli, transform_paths, does_exist=DEFAULT_TRANSFORM_EXISTS, timeout_seconds=DEFAULT_TIMEOUT_SECONDS):
+def assert_await_any_transforms_exist(cli, transform_refs, does_exist=DEFAULT_TRANSFORM_EXISTS, timeout_seconds=DEFAULT_TIMEOUT_SECONDS):
     """
     Asserts that we successfully awaited for any transforms to exist based on does_exist. If the timeout passes
     or the expression is_registered != actual state, then it will fail.
     :param cli:
-    :param transform_paths:
+    :param transform_refs:
     :param does_exist: (True | False) the state change we are waiting for.
     :param timeout_seconds: The amount of time to wait for a change before fail.
     :return:
     """
-    result = commands.await_any_transforms_exist(cli, transform_paths, does_exist, timeout_seconds)
+    result = commands.await_any_transforms_exist(cli, transform_refs, does_exist, timeout_seconds)
     assert result is True
     return result
 
 
-def assert_await_all_transforms_exist(cli, transform_paths, does_exist=DEFAULT_TRANSFORM_EXISTS, timeout_seconds=DEFAULT_TIMEOUT_SECONDS):
+def assert_await_all_transforms_exist(cli, transform_refs, does_exist=DEFAULT_TRANSFORM_EXISTS, timeout_seconds=DEFAULT_TIMEOUT_SECONDS):
     """
     Asserts that we successfully awaited for all transforms to exist based on does_exist. If the timeout passes
     or the expression is_registered != actual state, then it will fail.
     :param cli:
-    :param transform_paths:
+    :param transform_refs:
     :param does_exist: (True | False) the state change we are waiting for.
     :param timeout_seconds: The amount of time to wait for a change before fail.
     :return:
     """
-    result = commands.await_all_transforms_exist(cli, transform_paths, does_exist, timeout_seconds)
+    result = commands.await_all_transforms_exist(cli, transform_refs, does_exist, timeout_seconds)
     assert result is True
     return result
 
 
-def assert_await_renderer_visible(cli, transform_path, is_visible=DEFAULT_RENDERER_VISIBLE, timeout_seconds=DEFAULT_TIMEOUT_SECONDS):
+def assert_await_renderer_visible(cli, transform_ref, is_visible=DEFAULT_RENDERER_VISIBLE, timeout_seconds=DEFAULT_TIMEOUT_SECONDS):
     """
     Asserts that we successfully awaited for the renderer to be visible based on is_visible. If the timeout passes
     or the expression is_registered != actual state, then it will fail.
     :param cli:
-    :param transform_path:
+    :param transform_ref:
     :param is_visible: (True | False) the state change we are waiting for.
     :param timeout_seconds: The amount of time to wait for a change before fail.
     :return:
     """
-    result = commands.await_renderer_visible(cli, transform_path, is_visible, timeout_seconds)
+    result = commands.await_renderer_visible(cli, transform_ref, is_visible, timeout_seconds)
     assert result is True
     return result
 
@@ -105,33 +105,33 @@ def assert_await_scene_loaded(cli, scene_name, is_loaded=DEFAULT_SCENE_LOADED, t
     return result
 
 
-def assert_component_value_equals(cli, transform_path, component_type, name, value):
+def assert_component_value_equals(cli, transform_ref, component_type, name, value):
     """
     Asserts that component field or property equals to the provided value
     :param cli:
-    :param transform_path:
+    :param transform_ref:
     :param component_type: The C# type name of the component GetComponent(type)
     :param name: The field or property name.
     :param value: The value to check for equality (String | Number | Boolean)
     :return:
     """
 
-    result = commands.fetch_component_value(cli, transform_path, component_type, name)
+    result = commands.fetch_component_value(cli, transform_ref, component_type, name)
     assert result == value
     return result
 
 
-def assert_await_component_value_equals(cli, transform_path, component_type, name, value, timeout_seconds=DEFAULT_TIMEOUT_SECONDS):
+def assert_await_component_value_equals(cli, transform_ref, component_type, name, value, timeout_seconds=DEFAULT_TIMEOUT_SECONDS):
     """
     Asserts that we successfully awaited for the component field or property to become provided value.
     :param cli:
-    :param transform_path: The path of the transform where the component resides
+    :param transform_ref: Reference to the transform where the component resides
     :param component_type: The C# type name of the component GetComponent(type)
     :param name: The field or property name.
     :param value: The value to check for equality (String | Number | Boolean)
     :return:
     """
 
-    result = commands.await_component_value_equals(cli, transform_path, component_type, name, value, timeout_seconds)
+    result = commands.await_component_value_equals(cli, transform_ref, component_type, name, value, timeout_seconds)
     assert result is True
     return result
--- tests/test_transform_ref.py
+++ tests/test_transform_ref.py
@@ -4,15 +4,14 @@ from coeus_unity.transform import TransformRef
 
 class TransformRefTestCase(unittest.TestCase):
     def test_calculate_path(self):
-        transform_a = TransformRef("Some/Path")
-        transform_b = TransformRef("Other")
-        transform_b.add_child_ref(transform_a)
+        menu = TransformRef("Path/To/Menu")
 
-        assert transform_a.get_rendered_transform_path() == "Other/Some/Path"
+        button = TransformRef("Button", parent=menu)
+        assert button.transform_path == "Button"
+        assert button.get_relative_transform_path() == "Button"
+        assert button.get_absolute_transform_path() == "Path/To/Menu/Button"
 
-    def test_calculate_path_relative(self):
-        transform_a = TransformRef("Some/Path")
-        transform_b = TransformRef("Other")
-        transform_b.add_child_ref(transform_a)
-
-        assert transform_a.get_rendered_transform_path_relative(transform_b) == "Some/Path"
+        button.set_parent(None)
+        assert button.transform_path == "Path/To/Menu/Button"
+        assert button.get_relative_transform_path() == "Path/To/Menu/Button"
+        assert button.get_absolute_transform_path() == "Path/To/Menu/Button"
